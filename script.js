c8aed638d08662eb9f088330ef8f742ae5203392
ayen39 <ayenmayen162@gmail.com>
2026-04-16 12:50:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+function joinAlert() {
+  alert("You joined the Climate Action Movement 🌍");
+}
+
+const tips = [
+  "Plant trees around your home",
+  "Avoid burning waste",
+  "Use solar energy",
+  "Save water daily",
+  "Reduce plastic usage"
+];
+
+function showTip() {
+  const random = Math.floor(Math.random() * tips.length);
+  document.getElementById("tipText").innerText = tips[random];
+}
